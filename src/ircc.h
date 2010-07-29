cfd816c95534eda677635cb37dae94a8d318c48b
Alberto Cortés <alcortes@it.uc3m.es>
2010-07-29 14:30:01 +0200

    Add constants for message format (RFC2812 section 2.3)

--- src/ircc.h
+++ src/ircc.h
@@ -10,7 +10,12 @@ const int NICKNAME_MAX_LENGTH     = 9;  /* RFC2812 1.2.1, longer
                                            as no future evolutions of
                                            the protocol are supported by
                                            this software */
-const int CHANNELNAME_MAX_LENGTH  = 50; /* RFC2812 1.3 */
+const int CHANNELNAME_MAX_LENGTH  = 50;  /* RFC2812 1.3 */
+const int MESSAGE_MAX_PARAMS      = 15;  /* RFC2812 2.3 */
+const char PREFIX_PREFIX          = ':'; /* RFC2812 2.3 */
+const int MESSAGE_MAX_LENGTH      = 152; /* RFC2812 2.3 */
+const char MESSAGE_SEPARATOR      = ' '; /* RFC2812 2.3 */
+const std::string END_OF_MESSAGE("\r\n"); /* RFC2812 2.3 */
 
 // mix constants
 const bool DEF_DEBUG = false;
