a4e7f634d12eaba4d097129ce8af504e122dc82e
Alberto Cortés <alcortes@it.uc3m.es>
2010-07-29 13:35:41 +0200

    RC2812 section 1 constants

--- src/ircc.h
+++ src/ircc.h
@@ -4,7 +4,13 @@
 #include <string>
 
 // protocol constants
-const int SERVER_MAX_LENGTH = 63; /* RFC2812 1.1 */
+const int SERVERNAME_MAX_LENGTH   = 63; /* RFC2812 1.1 */
+const int NICKNAME_MAX_LENGTH     = 9;  /* RFC2812 1.2.1, longer
+                                           nicknames are not supported
+                                           as no future evolutions of
+                                           the protocol are supported by
+                                           this software */
+const int CHANNELNAME_MAX_LENGTH  = 50; /* RFC2812 1.3 */
 
 // mix constants
 const bool DEF_DEBUG = false;
