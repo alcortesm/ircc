d7fd82f3e2ee1a876e67f33625b1ab19bb7b0664
Alberto Cortés <alcortes@it.uc3m.es>
2010-08-03 20:08:02 +0200

    Move IRC protocol constants to new irc.h

---
+++ src/irc.h
@@ -0,0 +1,20 @@
+#ifndef IRC_H
+#define IRC_H
+
+#include <string>
+
+// protocol constants
+const int SERVERNAME_MAX_LENGTH   = 63; /* RFC2812 1.1 */
+const int NICKNAME_MAX_LENGTH     = 9;  /* RFC2812 1.2.1, longer
+                                           nicknames are not supported
+                                           as no future evolutions of
+                                           the protocol are supported by
+                                           this software */
+const int CHANNELNAME_MAX_LENGTH  = 50;  /* RFC2812 1.3 */
+const int MESSAGE_MAX_PARAMS      = 15;  /* RFC2812 2.3 */
+const char PREFIX_PREFIX          = ':'; /* RFC2812 2.3 */
+const int MESSAGE_MAX_LENGTH      = 152; /* RFC2812 2.3 */
+const char MESSAGE_SEPARATOR      = ' '; /* RFC2812 2.3 */
+const std::string END_OF_MESSAGE("\r\n"); /* RFC2812 2.3 */
+
+#endif
--- src/ircc.h
+++ src/ircc.h
@@ -1,22 +1,6 @@
 #ifndef IRCC_H
 #define IRCC_H
 
-#include <string>
-
-// protocol constants
-const int SERVERNAME_MAX_LENGTH   = 63; /* RFC2812 1.1 */
-const int NICKNAME_MAX_LENGTH     = 9;  /* RFC2812 1.2.1, longer
-                                           nicknames are not supported
-                                           as no future evolutions of
-                                           the protocol are supported by
-                                           this software */
-const int CHANNELNAME_MAX_LENGTH  = 50;  /* RFC2812 1.3 */
-const int MESSAGE_MAX_PARAMS      = 15;  /* RFC2812 2.3 */
-const char PREFIX_PREFIX          = ':'; /* RFC2812 2.3 */
-const int MESSAGE_MAX_LENGTH      = 152; /* RFC2812 2.3 */
-const char MESSAGE_SEPARATOR      = ' '; /* RFC2812 2.3 */
-const std::string END_OF_MESSAGE("\r\n"); /* RFC2812 2.3 */
-
 // mix constants
 const bool   DEF_DEBUG = false;
 const int    STDIN_FD = 0;
