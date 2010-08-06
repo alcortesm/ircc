c5b00e63997436df7a1798b8aff77f1648f21e21
Alberto Cortés <alcortes@it.uc3m.es>
2010-08-07 01:31:59 +0200

    Grow MAX_LINE_LEN to 1023

--- src/ircc.h
+++ src/ircc.h
@@ -6,6 +6,6 @@ const bool   DEF_DEBUG = false;
 const int    STDIN_FD = 0;
 const time_t MAIN_LOOP_TIMEOUT_SECS  = 2;
 const long   MAIN_LOOP_TIMEOUT_USECS = 0;
-const size_t MAX_LINE_LEN = 10;
+const size_t MAX_LINE_LEN = 1023;
 
 #endif
