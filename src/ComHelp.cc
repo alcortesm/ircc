eef06ddcc931973ad0c154ca4d186cb817e1f1cc
Alberto Cortés <alcortes@it.uc3m.es>
2010-09-13 16:45:01 +0200

    Update help message

--- src/ComHelp.cc
+++ src/ComHelp.cc
@@ -11,37 +11,44 @@ const std::string ComHelp::MSG("*** A typical user session goes like this:\n\
 ***     /auth alcortes\n\
 ***     /list\n\
 ***     /join #ro2\n\
-***     /who\n\
 ***     hello\n\
-***     /msg hello again\n\
-***     /join\n\
+***     /leave\n\
 ***     /disconnect\n\
 ***     /quit\n\
 *** \n\
 *** The full list of available commands are\n\
 *** \n\
-***     /connect <host> <port>   - connect to a server\n\
-***     /auth <nick>             - authenticate to the server.\n\
-***                                set nick, username & real name)\n\
-***     /list                    - list the channels in the server\n\
-***     /who                     - list users in your channel\n\
-***     /join [<channel>]        - join a channel or leave the channel\n\
-***                                if no arg.\n\
-***     /msg <txt>               - send <txt> to the channel\n\
-***     /disconnect              - disconnect from server\n\
-***     /quit                    - close program\n\
-***     /echo <txt>              - echoes <txt> back (debugging)\n\
-***     /nop                     - no operation, do nothing (debugging)\n\
-***     /sleep [<secs>]          - pause for <secs> seconds (debugging)\n\
 ***     /help                    - show this text\n\
 *** \n\
+***     /connect <host> <port>   - connect to a server\n\
+***\n\
+***     /auth <nick>             - authenticate with the server,\n\
+***                                sets nick, username & real name\n\
+***\n\
+***     /list                    - list available public channels\n\
+***\n\
+***     /who                     - list users in your channel\n\
+***\n\
+***     /join [<channel>]        - join a channel or tell what channel\n\
+***                                you are on\n\
+***\n\
+***     /msg <txt>               - send <txt> to the channel\n\
+***\n\
+***     /disconnect              - disconnect from server\n\
+***\n\
+***     /quit                    - close program\n\
+***\n\
+***     /nop [<text>]            - (debugging) no operation,\n\
+***                                just echoes <text> back\n\
+***\n\
+***     /sleep [<secs>]          - (debugging) pause for <secs> seconds\n\
+***\n\
 *** Lines from the user that don't begin with '/' are equivalent to\n\
 *** \n\
 ***     /msg line\n\
 *** \n\
-*** Lines beginning with \"*** \" are messages from the program to the user.\n\
-*** Lines beginning with \"<user> \" are messages from some user.\n\
-*** Lines beginning with \"<<< \" are from the server.\n\
+*** Lines beginning with \"*** \" are messages from the program.\n\
+*** Lines beginning with \"<user> \" are messages from <user>.\n\
 *** Lines beginning with \"DDD \" are debug messages from the program.\n\
 *** The rest of the lines are echoes of the user commands.");
 
